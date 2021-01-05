996286a97b7d229e5b7b33c43588159cd924af0c
Philipp Fritsche <ph.fritsche@gmail.com>
2021-01-05 12:05:26 +0100

    fix: make NPM_TOKEN optional

--- release.config.js
+++ release.config.js
@@ -1,7 +1,16 @@
 const config = {
-    'analyzeCommits': {
-        'preset': 'conventionalcommits',
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        '@semantic-release/github',
+    ],
+    analyzeCommits: {
+        preset: 'conventionalcommits',
     },
+}
+
+if (process.env.NPM_TOKEN) {
+    config.plugins.push('@semantic-release/npm')
 }
 
 module.exports = config
